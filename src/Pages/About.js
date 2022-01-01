edb2ee46a1991d06a490c164f571e3b8d6dff7b9
Della <humanita.1999@gmail.com>
2021-12-31 20:00:51 -0700

    created glass project cells

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function About() {
     return(
-        <div className="flex flex-col lg:flex-row h-screen justify-center lg:justify-start m-auto items-center ">
+        <div className="flex flex-col lg:flex-row h-screen justify-center lg:justify-start m-auto items-center">
             <h1 className="text-8xl md:text-9xl lg:basis-1/3 text-left">Hi!</h1>
             <p className="block lg:basis-2/3 text-lg md:text-xl lg:text-2xl p-8">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -2,8 +2,24 @@ import React from "react";
 
 function Projects() {
         return (
-            <div>
-                <h1 className="text-9xl">Projects</h1>
+            <div className="flex flex-col h-screen justify-center m-auto items-center gap-y-4">
+                <h1 className="text-7xl md:text-8xl text-left">Projects</h1>
+
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full p-5 h-1/2">
+                    <div className="bg-white shadow-xl rounded-lg overflow-hidden bg-opacity-20 h-full md:h-1/2 p-4">
+                        <h2>LifePlus</h2>
+                    </div>
+                    <div className="bg-white shadow-xl rounded-lg overflow-hidden bg-opacity-20 h-full md:h-1/2 p-4">
+                        <h2>Ada's Team Website</h2>
+                    </div>
+                    <div className="bg-white shadow-xl rounded-lg overflow-hidden bg-opacity-20 h-full md:h-1/2 p-4">
+                        <h2>Pocketbook</h2>
+                    </div>
+                    {/* Add NLP projects */}
+                    {/* <div className="Project">Grammar Checker</div> */}
+
+                </div>
+
             </div>
         )
 }
